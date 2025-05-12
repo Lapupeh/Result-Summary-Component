0dc6121fa105cc9bfa0eb11cd495ec971758d242
Olabimpe Abesin <abesinolabimpe@gmail.com>
2025-05-12 14:27:45 +0100

    initial commit

---
+++ Result Summary Component/README.md
@@ -0,0 +1,17 @@
+### Results summary component solution
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: Use the local JSON data to dynamically populate the content
+
+### Links
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Styled Components](https://styled-components.com/) - For styles
---
+++ Result Summary Component/src/App.jsx
@@ -0,0 +1,57 @@
+import data from './data.json';
+import './App.css';
+
+function ResultCard() {
+  return(
+    <>
+      <div className="result-card">
+        <h1>Your Result</h1>
+        <div className="total-score">
+          <span className='score'>76</span>
+          <span className='overall'> of 100</span>
+        </div>
+        <h2>Great</h2>
+        <p>You scored higher than 65% of the people who have taken these tests</p>
+      </div>
+    </>
+  ) 
+}
+
+function SummaryCard() {
+  return (
+    <>
+    <div className="summary-card">
+      <h1>Summary</h1>
+      <div className="test-summary">
+          {data.map((item, i) => (
+          <div className={`test test-${i}`}>
+            <div className="test-category">
+              <img src={item.icon} alt="icon"/>
+              <h4>{item.category}</h4>
+            </div>
+            <div>
+              <span className="test-score">{item.score}</span>
+              <span className='test-overall'> / 100</span>
+            </div>
+          </div>
+          ))}
+      </div>
+      <button>Continue</button>
+    </div>
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <>
+      <div className="result-summary-container">
+        <ResultCard/>
+        <SummaryCard/>
+      </div>
+    </>
+  )
+}
+
+export default App
